8d9fcc51874d855aaad9e55d23a06f79d84e7679
Ayushm010 <mohapatraayush587@gmail.com>
2025-08-05 15:51:29 +0530

    feat: all the js logic done

--- app.js
+++ app.js
@@ -33,28 +33,66 @@ const gameController = (function () {
 
   // lets run the game
   //1. we have to get the board and display it
-
   const board = gameboard.getBoard();
+
   let currentPlayer = player1;
+
   const startGame = () => {
     displayController.disp(board);//renders the board
 
-
-    for (let i = 0; i < 9; i++) {
+    let i;
+    for (i = 0; i < 9; i++) {
       const pos = Number(prompt(`enter the position ${currentPlayer.name}: `));
-      gameboard.setBoard(currentPlayer.marker, pos);
+
+      gameboard.setBoard(currentPlayer.marker, pos);//sets the current marker to the following position
       displayController.disp(board);
-      checkWinner(currentPlayer.marker);
+
+      if (checkWinner(currentPlayer.marker, pos)) {
+        console.log(currentPlayer.name + " is the Winner");
+        return;
+      }
       currentPlayer = currentPlayer.marker === "X" ? player2 : player1;
-
     }
+
+    if (i === 9) console.log("Draw");
   }
 
-  const checkWinner = (marker) => {
-    
+  const checkWinner = (marker, pos) => {
+    const posMap = {
+      0: [[0, 1, 2], [0, 3, 6], [0, 4, 8]],
+      1: [[0, 1, 2], [1, 4, 7]],
+      2: [[0, 1, 2], [2, 5, 8], [2, 4, 6]],
+      3: [[3, 4, 5], [0, 3, 6]],
+      4: [[3, 4, 5], [1, 4, 7], [0, 4, 8], [2, 4, 6]],
+      5: [[3, 4, 5], [2, 5, 8]],
+      6: [[6, 7, 8], [0, 3, 6], [2, 4, 6]],
+      7: [[6, 7, 8], [1, 4, 7]],
+      8: [[6, 7, 8], [2, 5, 8], [0, 4, 8]]
+    };
+
+    if (pos in posMap) {
+      const winnerArr = posMap[pos];
+       let flag = false;
+
+      for (let i = 0; i < winnerArr.length; i++) {
+        if (board[winnerArr[i][0]] === marker &&
+          board[winnerArr[i][1]] === marker &&
+          board[winnerArr[i][2]] === marker) {
+          flag = true;
+          return flag;
+        }
+      }
+    }
+
+    return false;
   }
 
-  return { startGame, };
+  const restartGame = () => {
+    gameboard.resetBoard();
+    startGame();    
+  }
+
+  return { startGame, restartGame };
 
 })();
 
